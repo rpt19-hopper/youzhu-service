52888ce43b0e2b00f7470079b7aa22b4cc02a00c
Youzhu <shiyouzhu89@gmail.com>
2020-06-01 02:53:53 -0700

    updated stress test script to include PUT requets

--- stressTestHelper.js
+++ stressTestHelper.js
@@ -1,5 +1,10 @@
-function getListingId(context, events, done) {
+const artilleryContext = (context, events, done) => {
   context.vars.id = Math.floor(Math.random() * 10000000);
+  context.vars.storeId = Math.floor(Math.random() * 10000000);
+  context.vars.userId = Math.floor(Math.random() * 10000000);
+  context.vars.text = "test test test"
+  context.vars.starRating = Math.floor(Math.random() * 5);
+
   return done();
 }
-module.exports = { getListingId }
+module.exports = { artilleryContext }
