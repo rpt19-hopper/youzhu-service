58fab1659008ce2f37138c71bd9072930e5b17a5
Youzhu <shiyouzhu89@gmail.com>
2020-06-26 03:24:27 -0700

    updated getProductReviews and getStoreReviews

--- client/src/ProductReviews.jsx
+++ client/src/ProductReviews.jsx
@@ -19,7 +19,7 @@ import './css/ProductReviews.scss';
 
 
 const ProductReviews = (props) => {
-  const server = 'http://ec2-18-223-151-174.us-east-2.compute.amazonaws.com:1234';
+  const server = 'http://localhost:1234';
   const [storeReviews, setStoreReviews] = useState([]);
   const [productReviews, setProductReviews] = useState([]);
   const [reviews, setReviews] = useState([]);
